78b226f987a3c865263079e0d4ce1ca602f20c5c
Gustavo Boaz <gustavo.boaz@hotmail.com>
2021-10-28 15:56:41 -0300

    @gustavoboaz[Task: CREATE UserModel]

---
+++ src/main/java/com/payboaz/App/models/UserModel.java
@@ -0,0 +1,68 @@
+package com.payboaz.App.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_users")
+public class UserModel {
+
+	private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long idUser;
+	private String name;
+	private String email;
+	private String password;
+	private String token;
+	private Float wallet;
+
+	public Long getIdUser() {
+		return idUser;
+	}
+
+	public void setIdUser(Long idUser) {
+		this.idUser = idUser;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public Float getWallet() {
+		return wallet;
+	}
+
+	public void setWallet(Float wallet) {
+		this.wallet = wallet;
+	}
+
+}
