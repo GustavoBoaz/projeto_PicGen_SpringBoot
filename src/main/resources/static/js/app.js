78b8272b29fb15848234cf5b5a414cd7c2811c2f
Gustavo Boaz <gustavo.boaz@hotmail.com>
2021-11-29 01:36:14 -0300

    @GustavoBoaz [Task: MODIFY relative path]

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,7 +1,7 @@
 (function() {
     if('serviceWorker' in navigator) {
         window.addEventListener('load', () => {
-          navigator.serviceWorker.register('/sw.js')
+          navigator.serviceWorker.register('/js/sw.js')
                    .then(function(registration) {
                    console.log('Service Worker Registered');
                    return registration;
